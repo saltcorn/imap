45059d72375db798f71ed68dc159911596f088ea
Tom Nielsen <tanielsen@gmail.com>
2023-03-27 19:45:30 +0100

    wip to db

--- sync-action.js
+++ sync-action.js
@@ -10,6 +10,17 @@ const objMap = (obj, f) => {
   });
   return result;
 };
+const get_max_uid = async (table_dest, uid_field) => {
+  const schema = db.getTenantSchemaPrefix();
+
+  const { rows } = await db.query(
+    `select max(${db.sqlsanitize(uid_field)}) from ${schema}"${db.sqlsanitize(
+      table_dest
+    )}"`
+  );
+  //console.log({ rows });
+  return rows[0].max;
+};
 
 module.exports = (cfg) => ({
   configFields: async () => {
@@ -21,6 +32,9 @@ module.exports = (cfg) => ({
     );
     const strFields = objMap(tableMap, (table) =>
       table.fields.filter((f) => f.type?.name === "String").map((f) => f.name)
+    );
+    const fileFields = objMap(tableMap, (table) =>
+      table.fields.filter((f) => f.type === "File").map((f) => f.name)
     );
     console.log("intFields", intFields);
     return [
@@ -59,16 +73,28 @@ module.exports = (cfg) => ({
           calcOptions: ["table_dest", strFields],
         },
       },
+      {
+        name: "file_field",
+        label: "File field",
+        type: "String",
+        attributes: {
+          calcOptions: ["table_dest", fileFields],
+        },
+      },
     ];
   },
-  /**
-   * @param {object} opts
-   * @param {object} opts.row
-   * @param {object} opts.configuration
-   * @param {object} opts.user
-   * @returns {Promise<void>}
-   */
-  run: async ({ row, configuration: { table_dest, file_field } }) => {
+
+  run: async ({
+    row,
+    configuration: {
+      table_dest,
+      uid_field,
+      file_field,
+      subj_field,
+      from_field,
+    },
+    req,
+  }) => {
     const client = new ImapFlow({
       host: cfg.host,
       port: 993,
@@ -79,20 +105,55 @@ module.exports = (cfg) => ({
       },
     });
     await client.connect();
-
+    const table = await Table.findOne({ name: table_dest });
     // Select and lock a mailbox. Throws if mailbox does not exist
     let lock = await client.getMailboxLock("INBOX");
+
+    //get max uid in db
+    const max_uid = await get_max_uid(table_dest, uid_field);
     try {
+      const hasAttachment = [];
+      let i = 0;
+      for await (let message of client.fetch(`${max_uid || 0}:*`, {
+        envelope: true,
+        bodyStructure: true,
+        uid: true,
+      })) {
+        console.log(`${message.uid}: ${message.envelope.subject}`);
+        await table.insertRow(
+          {
+            [uid_field]: message.uid,
+            [subj_field]: message.envelope.subject,
+            [from_field]: message.envelope.from[0].address,
+          },
+          req.user
+        );
+        const childNodes = (message.bodyStructure.childNodes || []).filter(
+          (cn) => cn.disposition === "attachment"
+        );
+        if (childNodes.length) {
+          hasAttachment.push({
+            uid: message.uid,
+            part: childNodes[0].part,
+          });
+        }
+        i++;
+        if (i > 10) break;
+      }
+      for (const { uid, part } of hasAttachment) {
+        let message = await client.fetchOne(
+          { uid },
+          {
+            bodyParts: [part],
+          }
+        );
+        const buf = message.bodyParts.get(part);
+      }
       // fetch latest message source
       // client.mailbox includes information about currently selected mailbox
       // "exists" value is also the largest sequence number available in the mailbox
       console.log("exists", 60659); //client.mailbox.exists);
-      let message = await client.fetchOne(client.mailbox.exists, {
-        envelope: true,
-        bodyStructure: true,
-        uid: true,
-        //bodyParts: ["2"],
-      });
+
       console.log("envelope", message.envelope);
       console.log("uid", message.uid);
       console.log("bodyStructure", message.bodyStructure);
